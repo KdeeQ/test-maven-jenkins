0ec54d0f064a41d58d961f1673399c0ec850512c
Qvickstream <kaj.qvickstrom@myy.haaga-helia.fi>
2014-04-16 16:21:02 +0300

    first commit

---
+++ src/main/java/fi/qvicsktream/hello/Hello.java
@@ -0,0 +1,18 @@
+package fi.qvicsktream.hello;
+
+import org.slf4j.LoggerFactory;
+
+
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		org.slf4j.Logger logger = LoggerFactory.getLogger(Hello.class);
+		logger.info("Hello Maven!");
+
+	}
+
+}
